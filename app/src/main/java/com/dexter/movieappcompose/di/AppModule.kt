03a57bec65620fde2e76c3d8e1e88c656d63526b
LAPTOP-78TVCNT3\Ibrahim <mubarokibrahim@gmail.com>
2023-05-22 14:20:49 +0700

    add chucker interceptor

--- app/src/main/java/com/dexter/movieappcompose/di/AppModule.kt
+++ app/src/main/java/com/dexter/movieappcompose/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.dexter.movieappcompose.di
 
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.chuckerteam.chucker.api.RetentionManager
 import com.dexter.movieappcompose.BuildConfig
 import com.dexter.movieappcompose.data.remote.services.MovieApiServices
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -19,9 +24,37 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun providesOkHttpClient(): OkHttpClient {
+    fun providesChuckerCollector(
+        @ApplicationContext context: Context
+    ): ChuckerCollector {
+        return ChuckerCollector(
+            context,
+            true,
+            RetentionManager.Period.ONE_HOUR
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideChuckerInterceptor(
+        @ApplicationContext context: Context,
+        collector: ChuckerCollector
+    ): ChuckerInterceptor {
+        return ChuckerInterceptor.Builder(context)
+            .collector(collector)
+            .maxContentLength(250_000)
+            .alwaysReadResponseBody(true)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(
+        chuckerInterceptor: ChuckerInterceptor
+    ): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .addInterceptor(chuckerInterceptor)
             .readTimeout(120, TimeUnit.SECONDS)
             .connectTimeout(120, TimeUnit.SECONDS)
             .build()
