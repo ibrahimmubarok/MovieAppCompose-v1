325eff995e2a3051c6ed1a311432f39964f498fd
LAPTOP-78TVCNT3\Ibrahim <mubarokibrahim@gmail.com>
2023-05-23 15:58:30 +0700

    remove redundant implement library

--- app/src/main/java/com/dexter/movieappcompose/presentation/MainActivity.kt
+++ app/src/main/java/com/dexter/movieappcompose/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -39,6 +40,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieApp(
     modifier: Modifier = Modifier,
